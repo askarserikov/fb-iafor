531c59d646502c7f18da5cbf8d1318700dabbc26
Askar Serikov <32137534+askarserikov@users.noreply.github.com>
2019-01-10 00:19:15 +0600

    fixin hm 3

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 // Imports dependencies and set up http server
 const
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  app = express().use(bodyParser.json()), // creates express http server
   request = require('request');
 
 // Sets server port and logs message on success
